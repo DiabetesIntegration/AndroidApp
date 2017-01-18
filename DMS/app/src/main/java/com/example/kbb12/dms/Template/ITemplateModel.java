0ba1f141a64c8db340cb0c841751e2d74a2e7548
Greg Law <Greg Law>
2017-01-18 13:28:30 +0000

    Combined the view and activity.
    Compromises MVC but does make accessing necessary functionality
    far easier.

--- DMS/app/src/main/java/com/example/kbb12/dms/Template/ITemplateModel.java
+++ DMS/app/src/main/java/com/example/kbb12/dms/Template/ITemplateModel.java
@@ -1,6 +1,8 @@
 package com.example.kbb12.dms.Template;
 
 
+import com.example.kbb12.dms.StartUp.ModelObserver;
+
 /**
  * Created by kbb12 on 17/01/2017.
  */
@@ -8,4 +10,5 @@ public interface ITemplateModel {
 
     public String getExampleData();
     public void setExampleData(String newData);
+    public void registerObserver(ModelObserver obs);
 }
