029a040f02d65a1882ffb66921f41387e872f0c5
ciaran <ciaran-mcfadden@hotmail.co.uk>
2017-04-17 11:04:24 +0100

    added in unit testing for half of unit models and doing ingreient/meal

---
+++ DMS/app/src/test/java/com/example/kbb12/dms/database/mealPlannerRecord/IngredientTest.java
@@ -0,0 +1,72 @@
+package com.example.kbb12.dms.database.mealPlannerRecord;
+
+import com.example.kbb12.dms.reusableFunctionality.baseScreen.view.ModelObserver;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ciaran on 4/17/2017.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class IngredientTest {
+    @Mock
+    Ingredient model;
+    String name;
+    Double amPerHundred;
+    Integer pWeight;
+    String barcode;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        name = "pasta sauce";
+        amPerHundred = 20.0;
+        pWeight = 100;
+        barcode = "19403866382";
+        model = new Ingredient(name,amPerHundred,pWeight);
+    }
+
+
+
+    @Test
+    public void getName() throws Exception {
+        assertEquals(name, model.getName());
+    }
+
+    @Test
+    public void getCarbsPerHundredG() throws Exception {
+        assertEquals(amPerHundred,model.getCarbsPerHundredG());
+    }
+
+    @Test
+    public void getPacketWeight() throws Exception {
+        assertEquals(pWeight,model.getPacketWeight());
+    }
+
+    @Test
+    public void getBarcode() throws Exception {
+        assertEquals(null,model.getBarcode());
+        amPerHundred = 50.0;
+
+        model = new Ingredient(name,amPerHundred,pWeight,barcode);
+        assertEquals(name,model.getName());
+        assertEquals(amPerHundred,model.getCarbsPerHundredG());
+        assertEquals(pWeight,model.getPacketWeight());
+        assertEquals(barcode,model.getBarcode());
+
+
+    }
+
+    @Test
+    public void getCustom() throws Exception {
+        
+    }
+
+}
